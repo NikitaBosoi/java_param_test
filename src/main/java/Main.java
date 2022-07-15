81e06add11f51da84ad93738ebcf62ddf13f10da
Nikita Bosoi <nikitabosoi@yandex.ru>
2022-07-15 17:57:01 +0300

    my parameterized test

---
+++ src/main/java/Main.java
@@ -0,0 +1,32 @@
+@RunWith(Parameterized.class)
+public class CheckIsAdultTest {
+
+    private final int age;
+    private final boolean result;
+
+    public CheckIsAdultTest(int age, boolean result) {
+        // Инициализируй поля класса в конструкторе
+        this.age = age;
+        this.result = result;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] getTextData() {
+        return new Object[][] {
+                // Заполни массив тестовыми данными и ожидаемым результатом
+                {19, true},
+                {17, false},
+                {18, true},
+                {21, true}
+        };
+    }
+
+    @Test
+    public void checkIsAdultWhenAgeThenResult() {
+        Program program = new Program();
+        // Передай сюда возраст пользователя
+        boolean isAdult = program.checkIsAdult(age);
+        // Сравни полученный и ожидаемый результаты, не забудь про сообщение об ошибке
+        assertEquals("Ответ неправильный", result, isAdult);
+    }
+}
